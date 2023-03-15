bb99bc759a9240ae2858da95f7d178558f416420
Dion <lordcryer@yahoo.com>
2023-03-15 14:32:44 -0500

    Drop phone broke camera

--- SmartPhone.cs
+++ SmartPhone.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Technology
 {
-    public class SmartPhone
+    public class SmartPhone : Computer
     {
+        public bool HasCamera { get; set; } = true;
+
+        public SmartPhone(bool hasCamera, double ram, double screenSize, string oS) : base(ram, screenSize, oS)
+        {
+            HasCamera = hasCamera;
+        }
+
+        public void DropPhone()
+        {
+            HasCamera = false;
+        }
     }
 }
